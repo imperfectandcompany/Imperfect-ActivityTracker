b9d56ab860ae3c1036cfaca10b60f788b174143b
raz <9876747+razpbrry@users.noreply.github.com>
2024-03-09 12:05:29 -0600

    Create server and map time data classes

--- src/Player/TimeData.cs
+++ src/Player/TimeData.cs
@@ -5,5 +5,22 @@
         public Dictionary<string, DateTime> Times { get; set; } = new Dictionary<string, DateTime>();
 
         public Dictionary<string, int> TimeFields { get; set; } = new Dictionary<string, int>();
+
+        public List<ServerTimeData> ServerTimeDataList { get; set; } = new();
+    }
+
+    public class ServerTimeData
+    {
+        public List<MapTimeData> Maps { get; set; } = new();
+        public int TotalSurfingTime { get; set; }
+        public int TotalSpecTime { get; set; }
+    }
+
+    public class MapTimeData
+    {
+        public string MapName { get; set; } = "";
+        public string WorkshopID { get; set; } = "";
+        public int SurfingTime { get; set; }
+        public int SpecTime { get; set; }
     }
 }
