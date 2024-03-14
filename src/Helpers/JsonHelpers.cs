5344c9e7cbf6f8850ab3c9af640bad253aa65222
raz <9876747+razpbrry@users.noreply.github.com>
2024-03-13 23:33:36 -0500

    Create json de/serialize helper mthds, create snake case helper mthd

---
+++ src/Helpers/JsonHelpers.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace ImperfectActivityTracker.Helpers
+{
+    public static class JsonHelpers
+    {
+        public static JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = SnakeCaseNamingPolicy.Instance
+        };
+
+        public static T DeserializeJson<T>(string jsonData)
+        {
+            T? deserializedJson = default;
+
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                try
+                {
+                    deserializedJson = JsonSerializer.Deserialize<T>(jsonData, _jsonSerializerOptions);
+                }
+                catch (Exception ex)
+                {
+                    ImperfectActivityTracker._logger.LogError("An error occurred deserializing the JSON: {message}", ex.Message);
+                }
+            }
+            else
+            {
+
+            }
+
+
+            return deserializedJson;
+        }
+
+        public static string SerializeJson(object objecToSerialize)
+        {
+            string serializedJson = "";
+
+            try
+            {
+                serializedJson = JsonSerializer.Serialize(objecToSerialize, _jsonSerializerOptions);
+            }
+            catch (Exception ex)
+            {
+                ImperfectActivityTracker._logger.LogError("An error occurred serializing the JSON: {message}", ex.Message);
+            }
+
+            return serializedJson;
+        }
+    }
+
+    public class SnakeCaseNamingPolicy : JsonNamingPolicy
+    {
+        public static SnakeCaseNamingPolicy Instance { get; } = new SnakeCaseNamingPolicy();
+
+        public override string ConvertName(string name)
+        {
+            return name.ToSnakeCase();
+        }
+    }
+}
---
+++ src/Helpers/TextHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace ImperfectActivityTracker.Helpers
+{
+    public static class TextHelpers
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+    }
+}
