595f2890c8b456e7496242ede3ef188ac597eaf0
raz <9876747+razpbrry@users.noreply.github.com>
2024-03-13 23:29:01 -0500

    Change server ip list prop to single server ip

--- src/Configuration/Config.cs
+++ src/Configuration/Config.cs
@@ -5,14 +5,11 @@ namespace ImperfectActivityTracker.Configuration
 {
     public class Config : BasePluginConfig
     {
-        /// <summary>
-        /// Config version number
-        /// </summary>
         [JsonPropertyName("ConfigVersion")]
         public override int Version { get; set; } = 2;
 
         public DatabaseSettings DatabaseSettings { get; set; } = new();
 
-        public List<string> ServerIpList { get; set; } = new();
+        public string ServerIp { get; set; } = "";
     }
 }
